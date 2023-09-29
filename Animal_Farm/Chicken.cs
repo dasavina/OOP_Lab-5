38ad7ef8c62fdcfe081a68aa7734c1338b9ad4ac
Savina Daria <savina.dariia@chnu.edu.ua>
2023-09-29 23:01:54 +0300

    2 start

---
+++ Animal_Farm/Chicken.cs
@@ -0,0 +1,14 @@
+﻿class Chicken
+{
+    public string name
+    {
+        get { return name; }
+        set { if (value is null) { throw new Exception("name can't be null"); } else {name = value; } }
+    }
+
+    public int age
+    {
+        get { return age; }
+        private set { if (value > 0) { age = value; } else { throw new Exception("Age cannot be 0 or less"); } }
+    }
+}

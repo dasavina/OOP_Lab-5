7f086ab4fa529fcee9449e49362231b3a682951c
Savina Daria <savina.dariia@chnu.edu.ua>
2023-09-29 21:43:16 +0300

    1

---
+++ Class_box/Box.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+class Box
+{
+    double h { get; set; }
+    double w { get; set; }
+    double l { get; set; }
+
+    public Box(double h, double w, double l)
+    {
+        this.h = h;
+        this.w = w;
+        this.l = l;
+    }
+
+    public double Volume()
+    {
+        return h * w * l;
+    }
+
+    public double Surface()
+    {
+        return 2 * (h * w + w * l + h * l);
+    }
+
+    public double LateralSurface()
+    {
+        return h * l + h * w;
+    }
+}

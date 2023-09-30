0ff85dfe97bebc8d3fdc5dbf0f0b239981a6c72f
Savina Daria <savina.dariia@chnu.edu.ua>
2023-09-30 11:54:06 +0300

    task 3

--- Animal_Farm/Chicken.cs
+++ Animal_Farm/Chicken.cs
@@ -3,12 +3,30 @@
     public string name
     {
         get { return name; }
-        set { if (value is null) { throw new Exception("name can't be null"); } else {name = value; } }
+        set { if (value is null || value.Equals("")|| value.Equals(" ")) { throw new Exception("name can't be empty"); } else {name = value; } }
     }
 
     public int age
     {
         get { return age; }
-        private set { if (value > 0) { age = value; } else { throw new Exception("Age cannot be 0 or less"); } }
+        private set { if (value > 0 && value <15) { age = value; } else { throw new Exception("Age must be in range of 0 to 15"); } }
+    }
+
+    public Chicken(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    private int CalculateProductPerDay()
+    {
+        Random r = new System.Random();
+        return r.Next(0, 7);
+    }
+
+    public string getProductPerDay()
+    {
+        string result = ("Chicken " + name+ " (age " + age  + ") can produce " + CalculateProductPerDay() + " eggs per day.");
+        return result;
     }
 }
